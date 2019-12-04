1e29c3914ffe6f6f0e98a28cd2b29e545810c6ef
bradocchs <bradspencer@covenantchristian.org>
2019-12-03 21:58:43 -0500

    removed xml-js

--- spaces.js
+++ spaces.js
@@ -1,5 +1,4 @@
 const https = require('https')
-const convert = require('xml-js')
 
 https.get('https://covenant.nyc3.digitaloceanspaces.com/', (res) => {
   let body = ''
@@ -7,22 +6,7 @@ https.get('https://covenant.nyc3.digitaloceanspaces.com/', (res) => {
     body += chunk
   })
   res.on('end', () => {
-    //console.log(body)
     const files = Array.from(body.matchAll(RegExp('library-tv/[^<]*\.jpg', 'g')), m => m[0])
-    // const files = []
-    // const data = convert.xml2js(body)
-    // for (const e of data.elements[0].elements) {
-    //   for (const key in e) {
-    //     if (e.hasOwnProperty(key)) {
-    //       const element = e[key]
-    //       if (element[0].elements) {
-    //         if (element[0].elements[0].text.includes('library-tv') && element[0].elements[0].text.length > 11) {
-    //           files.push(element[0].elements[0].text.substring(11))
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
     console.log(files)
   })
 }).on('error', (e) => {
