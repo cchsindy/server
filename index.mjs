f1c347314b3d22ff7802adb02bae1a42f5d426f9
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-12-06 11:41:32 -0500

    added spaces service

---
+++ index.mjs
@@ -0,0 +1,19 @@
+import express from 'express'
+import { SpacesService } from 'services'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.get('/', (req, res) => res.send('Covenant Education'))
+app.get('/library-tv', (req, res) => res.sendFile('/home/brad/server/library-tv.html'))
+app.post('/spaces', (req, res) => {
+  const spaces = new SpacesService
+  spaces.getFileList(req.params.directory)
+  .then(list => {
+    res.json(JSON.stringify(list))
+  })
+})
+
+app.listen(port, () => console.log(`Server listening on port ${port}...`))
