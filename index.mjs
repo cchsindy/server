4a182b25b4bec39ad1b20aefaeb46c9f417dfde9
bradocchs <bradspencer@covenantchristian.org>
2019-12-08 15:04:41 -0500

    json fix

--- index.mjs
+++ index.mjs
@@ -6,12 +6,13 @@ const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(express.json())
 app.get('/', (req, res) => res.send('Covenant Education'))
 app.get('/library-tv', (req, res) => res.sendFile('/home/brad/server/library-tv.html'))
 app.get('/taylor-tv', (req, res) => res.sendFile('/home/brad/server/taylor-tv.html'))
 app.post('/spaces', (req, res) => {
   const spaces = new SpacesService
-  spaces.getFileList(req.params.directory)
+  spaces.getFileList(req.body.directory)
   .then(list => {
     res.json(list)
   })
