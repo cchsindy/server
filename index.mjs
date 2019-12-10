120aef887e796f08848ef0ef6d62f25fe5dfd006
Brad Spencer <bradspencer@Brads-Mac-mini.local>
2019-12-10 16:18:07 -0500

    added firestore

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import express from 'express'
-import { SpacesService } from 'services'
+import { FirestoreService, SpacesService } from 'services'
 import cors from 'cors'
 
 const app = express()
@@ -10,6 +10,13 @@ app.use(express.json())
 app.get('/', (req, res) => res.send('Covenant Education'))
 app.get('/library-tv', (req, res) => res.sendFile('/home/brad/server/library-tv.html'))
 app.get('/taylor-tv', (req, res) => res.sendFile('/home/brad/server/taylor-tv.html'))
+app.post('/firestore', (req, res) => {
+  const fs = new FirestoreService
+  fs.getAnnouncements()
+  .then(list => {
+    res.json(list)
+  })
+})
 app.post('/spaces', (req, res) => {
   const spaces = new SpacesService
   spaces.getFileList(req.body.directory)
