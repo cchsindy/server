c7724e96615d5e01d7ecc4ea869d3ae0906e9340
Brad Spencer <bradspencer@Brads-Mac-mini.attlocal.net>
2020-05-20 11:16:50 -0400

    added closet data

--- index.mjs
+++ index.mjs
@@ -8,20 +8,37 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => res.send('Covenant Education'))
-app.get('/announcements-tv', (req, res) => res.sendFile('/Users/bradspencer/Node/server/announcements-tv.html'))
-app.get('/library-tv', (req, res) => res.sendFile('/home/brad/server/library-tv.html'))
-app.get('/taylor-tv', (req, res) => res.sendFile('/home/brad/server/taylor-tv.html'))
+app.get('/announcements-tv', (req, res) =>
+  res.sendFile('/Users/bradspencer/Node/server/announcements-tv.html')
+)
+app.get('/closet-data', (req, res) =>
+  // res.sendFile('/Users/bradspencer/Node/server/closet-data.html')
+  res.sendFile('/home/brad/server/closet-data.html')
+)
+app.get('/library-tv', (req, res) =>
+  res.sendFile('/home/brad/server/library-tv.html')
+)
+app.get('/taylor-tv', (req, res) =>
+  res.sendFile('/home/brad/server/taylor-tv.html')
+)
 app.post('/firestore', (req, res) => {
-  const fs = new FirestoreService
-  fs.getAnnouncements()
-    .then(list => {
-      res.json(list)
-    })
+  const fs = new FirestoreService()
+  switch (req.body.method) {
+    case 'get-announcements':
+      fs.getAnnouncements().then((list) => {
+        res.json(list)
+      })
+      break
+    case 'get-closet-data':
+      fs.getClosetData().then((list) => {
+        res.json(list)
+      })
+      break
+  }
 })
 app.post('/spaces', (req, res) => {
-  const spaces = new SpacesService
-  spaces.getFileList(req.body.directory)
-  .then(list => {
+  const spaces = new SpacesService()
+  spaces.getFileList(req.body.directory).then((list) => {
     res.json(list)
   })
 })
